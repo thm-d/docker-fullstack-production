d523e16a1be74afb7763d7ce2191f18aad5e2580
thm-d <thomas.dauriac@gmail.com>
2023-07-11 14:42:20 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      try {
+        const response = await fetch("/api/count");
+        if (response.ok) {
+          setCount(await response.json());
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCount();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Count : {count}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
